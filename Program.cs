bedc9f48e52b481fed9d8c1f08369f395d5fb1f3
Viney <vineykhaneja999@gmail.com>
2022-02-08 18:12:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computation
+{
+    internal class Arithmatic
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+        }
+    }
+}
