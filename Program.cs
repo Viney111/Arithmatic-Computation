f64ab0bc285917955fd5d03b8d4a197281bdbb11
Viney <vineykhaneja999@gmail.com>
2022-02-08 18:19:17 +0530

    [Viney] FEATURE ADDED: a + b * c

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace Computation
             int b = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter c: ");
             int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"a+b*c is { a + b * c}");
             Console.ReadLine();
 
         }
